e932b88132989aea2bde81a4e70785e1cfec1881
Sylvain CAU <ashdevfr@gmail.com>
2016-06-09 10:35:20 -0700

    feat: Add airPopover & Hint features

---
+++ src/js/vanilla/module/AirPopover.js
@@ -0,0 +1,62 @@
+define([
+  'summernote/base/core/agent',
+  'summernote/base/core/func',
+  'summernote/base/core/list',
+  'summernote/base/core/dom'
+], function (agent, func, list, dom) {
+  var AirPopover = function (context) {
+    var self = this;
+
+    var options = context.options;
+
+    this.events = {
+      'summernote.keyup summernote.mouseup summernote.scroll': function () {
+        self.update();
+      },
+      'summernote.change summernote.dialog.shown': function () {
+        self.hide();
+      },
+      'summernote.focusout': function (we, e) {
+        // [workaround] Firefox doesn't support relatedTarget on focusout
+        //  - Ignore hide action on focus out in FF.
+        if (agent.isFF) {
+          return;
+        }
+
+        if (!e.relatedTarget || !dom.ancestor(e.relatedTarget, func.eq(self.$popover[0]))) {
+          self.hide();
+        }
+      }
+    };
+
+    this.shouldInitialize = function () {
+      return options.airMode &&
+        options.popover &&
+        options.popover.air &&
+        typeof options.popover.air.hide === 'function' &&
+        typeof options.popover.air.update === 'function';
+    };
+
+    this.initialize = function () {
+    };
+
+    this.update = function () {
+      var styleInfo = context.invoke('editor.currentStyle');
+      if (styleInfo.range && !styleInfo.range.isCollapsed()) {
+        var rect = list.last(styleInfo.range.getClientRects());
+        if (rect) {
+          var bnd = func.rect2bnd(rect);
+          options.popover.air.update(styleInfo, bnd);
+        }
+      } else {
+        this.hide();
+      }
+    };
+
+    this.hide = function () {
+      options.popover.air.hide();
+    };
+  };
+
+  return AirPopover;
+});
