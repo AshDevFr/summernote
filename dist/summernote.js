1320e53fe51487b6f18d62af58e136912e4ea5fb
Sylvain CAU <ashdevfr@gmail.com>
2016-06-09 17:36:34 -0700

    chore: new release

--- dist/summernote.js
+++ dist/summernote.js
@@ -1,12 +1,12 @@
 /**
- * Super simple wysiwyg editor v0.8.3
+ * Super simple wysiwyg editor v0.8.4
  * http://summernote.org/
  *
  * summernote.js
  * Copyright 2013-2016 Alan Hong. and other contributors
  * summernote may be freely distributed under the MIT license./
  *
- * Date: 2016-06-10T00:34Z
+ * Date: 2016-06-10T00:50Z
  */
 (function (factory) {
   /* global define */
@@ -5082,7 +5082,7 @@
   };
 
   $.summernote = $.extend($.summernote, {
-    version: '0.8.3',
+    version: '0.8.4',
     ui: ui,
     dom: dom,
 
