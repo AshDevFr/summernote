d05b11c1e977fe477161cba54932eb3bef0fff47
Sylvain CAU <ashdevfr@gmail.com>
2016-10-26 14:21:34 -0700

    feat(airPopover): Add toggleList with paragraph normalize

--- src/js/vanilla/module/AirPopover.js
+++ src/js/vanilla/module/AirPopover.js
@@ -207,6 +207,26 @@ define([
       removeFormat(rng, dom.isBlockquote);
     };
 
+    this.insertOrderedList = function (rng) {
+      self.toggleList(rng, 'insertOrderedList');
+    };
+
+    this.insertUnorderedList = function (rng) {
+      self.toggleList(rng, 'insertUnorderedList');
+    };
+
+    this.toggleList = function (rng, command) {
+      if (!command) {
+        return;
+      }
+      rng = self.getFullParaRange(rng);
+      rng = self.createPara(rng);
+      rng = unWrapBR(rng);
+      rng = normalizePara(rng);
+      rng.select();
+      context.invoke('editor.' + command);
+    };
+
     this.splitPara = function (rng) {
       if (!$editable.is(':focus')) {
         $editable.focus();
@@ -314,6 +334,195 @@ define([
       }
     };
 
+    function normalizePara(rng) {
+      if (!$editable.is(':focus')) {
+        $editable.focus();
+      }
+      rng = rng || self.lastRange || range.create(editable);
+      var rngSave = saveRng(rng);
+
+      var nodes = rng.nodes().filter(function (node) {
+        if (node.parentNode && node.parentNode.childNodes && dom.position(node) > 0) {
+          return isWrappable(node) && !isWrappable(node.parentNode.childNodes[dom.position(node) - 1]);
+        } else {
+          return isWrappable(node);
+        }
+      });
+
+      if (nodes.length) {
+        nodes.forEach(function (node) {
+          var nodeList = [],
+              parentNode = node.parentNode,
+              childNodes = list.from(parentNode.childNodes),
+              isBreak = false,
+              position = {container: parentNode},
+              i;
+
+          for (i = dom.position(node); i < childNodes.length; i++) {
+            var child = childNodes[i];
+            if (!isBreak && isWrappable(child)) {
+              nodeList.push(child);
+            } else if (!isBreak) {
+              isBreak = true;
+              position.rightNode = child;
+            }
+          }
+
+          if (isBreak && !dom.position(node) || dom.position(node)) {
+            wrapNodes(nodeList, position, rngSave);
+          }
+        });
+      }
+
+      return restoreRng(rngSave);
+
+      function isWrappable(node) {
+        return !isPara(node) &&
+          !isVoid(node) &&
+          node.parentNode &&
+          (isPara(node.parentNode) || dom.isEditable(node.parentNode));
+      }
+
+      function wrapNodes(nodes, position, rngSave) {
+        if (!nodes.length) {
+          return;
+        }
+
+        var node = document.createElement('div');
+
+        nodes.forEach(function (child, index) {
+          if (child.parentNode === rngSave.nextPoint.node && dom.position(child) < rngSave.nextPoint.offset) {
+            rngSave.nextPoint.offset -= 1;
+          } else if (child.parentNode === rngSave.nextPoint.node && dom.position(child) === rngSave.nextPoint.offset) {
+            rngSave.nextPoint = {
+              node: node,
+              offset: index
+            };
+          }
+          node.appendChild(child);
+        });
+
+        if (position.rightNode && position.rightNode.parentNode) {
+          if (position.rightNode.parentNode === rngSave.nextPoint.node && dom.position(position.rightNode) < rngSave.nextPoint.offset) {
+            rngSave.nextPoint.offset += 1;
+          }
+          position.rightNode.parentNode.insertBefore(node, position.rightNode);
+          if (dom.isBR(position.rightNode)) {
+            if (position.rightNode.parentNode === rngSave.nextPoint.node && dom.position(position.rightNode) < rngSave.nextPoint.offset) {
+              rngSave.nextPoint.offset -= 1;
+            }
+            position.rightNode.parentNode.removeChild(position.rightNode);
+          }
+        } else {
+          if (!position.container || !self.isAncestor(position.container, editable)) {
+            if (editable === rngSave.nextPoint.node &&
+              rngSave.nextPoint.offset === editable.childNodes.length) {
+              rngSave.nextPoint.offset += 1;
+            }
+            editable.appendChild(node);
+          } else {
+            if (position.container === rngSave.nextPoint.node &&
+              rngSave.nextPoint.offset === position.container.childNodes.length) {
+              rngSave.nextPoint.offset += 1;
+            }
+            position.container.appendChild(node);
+          }
+        }
+
+        return node;
+      }
+    }
+
+    function unWrapBR(rng) {
+      if (!$editable.is(':focus')) {
+        $editable.focus();
+      }
+      rng = rng || self.lastRange || range.create(editable);
+      var rngSave = saveRng(rng);
+
+      var nodes = rng.nodes().filter(function (node) {
+        var point = {
+          node: node.parentNode,
+          offset: dom.position(node)
+        };
+        return dom.isBR(node) && !isPara(node.parentNode) && (!dom.isLeftEdgePoint(point) || !dom.isRightEdgePoint(point));
+      });
+
+      if (!nodes || !nodes.length) {
+        return rng;
+      }
+
+      return processBRNodes(nodes);
+
+      function processBRNodes(nodes) {
+        nodes.forEach(function (node) {
+          var parentPoint = {
+            node: node.parentNode,
+            offset: dom.position(node)
+          };
+          var point = {
+            node: node,
+            offset: 0
+          };
+
+          if (dom.isLeftEdgePoint(parentPoint) && !dom.isRightEdgePoint(parentPoint) && !dom.isEditable(parentPoint.node)) {
+            parentPoint.node.parentNode.insertBefore(node, parentPoint.node);
+          } else if (dom.isRightEdgePoint(parentPoint) && !dom.isLeftEdgePoint(parentPoint) && !dom.isEditable(parentPoint.node)) {
+            var parentParentPoint = {
+              node: parentPoint.node.parentNode,
+              offset: dom.position(parentPoint.node)
+            };
+            if (dom.isRightEdgePoint(parentParentPoint)) {
+              if (parentParentPoint.node === rngSave.nextPoint.node &&
+                rngSave.nextPoint.offset === parentParentPoint.node.childNodes.length) {
+                rngSave.nextPoint.offset += 1;
+              }
+              parentParentPoint.node.appendChild(node);
+            } else {
+              var rightNode = parentParentPoint.node.childNodes[dom.position(parentPoint.node) + 1];
+              if (parentParentPoint.node === rngSave.nextPoint.node &&
+                rngSave.nextPoint.offset >= dom.position(rightNode)) {
+                rngSave.nextPoint.offset += 1;
+              }
+              parentParentPoint.node.insertBefore(node, rightNode);
+            }
+          } else {
+            var prevPoint = dom.prevPoint(point);
+            if (prevPoint) {
+              var parentNode = prevPoint.node.parentNode;
+              if (parentNode === rngSave.nextPoint.node &&
+                rngSave.nextPoint.offset >= dom.position(prevPoint.node)) {
+                rngSave.nextPoint.offset += 1;
+              }
+              var info = splitNode(prevPoint);
+              if (info.rightNode && info.rightNode.parentNode) {
+                if (info.rightNode.parentNode === rngSave.nextPoint.node &&
+                  rngSave.nextPoint.offset >= dom.position(info.rightNode)) {
+                  rngSave.nextPoint.offset += 1;
+                }
+                info.rightNode.parentNode.insertBefore(node, info.rightNode);
+              }
+            }
+          }
+        });
+
+        var rng = restoreRng(rngSave);
+
+        nodes = rng.nodes().filter(function (node) {
+          var point = {
+            node: node.parentNode,
+            offset: dom.position(node)
+          };
+          return dom.isBR(node) && !isPara(node.parentNode) && (!dom.isLeftEdgePoint(point) || !dom.isRightEdgePoint(point));
+        });
+
+        if (nodes && nodes.length) {
+          return processBRNodes(nodes);
+        }
+        return rng;
+      }
+    }
+
     function removeFormat(rng, pred) {
       if (!pred) {
         return;
@@ -346,6 +555,26 @@ define([
           }
         });
       }
+    }
+
+    function splitNode(point) {
+      var leftRange = range.create(editable).nativeRange();
+      leftRange.setStart(point.node.parentNode, dom.position(point.node));
+      leftRange.setEnd(point.node, point.offset);
+      var leftContent = leftRange.extractContents();
+      var rightNode;
+
+      if (list.from(point.node.parentNode.childNodes).length) {
+        rightNode = point.node;
+        point.node.parentNode.insertBefore(leftContent, rightNode);
+      } else {
+        point.node.parentNode.appendChild(leftContent);
+      }
+
+      return {
+        container: point.node.parentNode,
+        rightNode: rightNode
+      };
     }
 
     function splitPoint(point) {
@@ -430,6 +659,32 @@ define([
         container: editable,
         rightNode: rightNode
       };
+    }
+
+    function isVoid(node) {
+      return node && node.nodeName && /^BR|^HR|^IFRAME/.test(node.nodeName.toUpperCase());
+    }
+
+    function isPara(node) {
+      return dom.isPara(node) || dom.isBlockquote(node);
+    }
+
+    function saveRng(rng) {
+      return {
+        prevPoint: dom.prevPoint(rng.getStartPoint()),
+        nextPoint: dom.nextPoint(rng.getEndPoint())
+      };
+    }
+
+    function restoreRng(save) {
+      var startPoint = save.prevPoint ?
+      dom.nextPoint(save.prevPoint) || {node: editable, offset: 0} :
+      {node: editable, offset: 0};
+      var endPoint = save.nextPoint ?
+      dom.prevPoint(save.nextPoint) || {node: editable, offset: editable.childNodes.length} :
+      {node: editable, offset: editable.childNodes.length};
+
+      return range.create(startPoint.node, startPoint.offset, endPoint.node, endPoint.offset);
     }
   };
 
