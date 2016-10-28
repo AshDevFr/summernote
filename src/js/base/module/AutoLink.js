672100094dcb1f51d48a27af9f98ec525990d6bd
Sylvain CAU <ashdevfr@gmail.com>
2016-10-28 14:15:04 -0700

    fix(autoLink): Fix link pattern

--- src/js/base/module/AutoLink.js
+++ src/js/base/module/AutoLink.js
@@ -7,7 +7,7 @@ define([
 ], function (func, list, dom, range, key) {
   var AutoLink = function (context) {
     var self = this;
-    var linkPattern = /^((http|https|ftp|mailto):\/\/[^\s\.]+)$/,
+    var linkPattern = /^((http|https|ftp|mailto):\/\/([^\s\.\/]+\.){1,2}([^\s\.\/]+)([^\s\.]*))$/,
         httpPattern = /^(www\.\w+\.[a-z]{2,3}[\w+\/\?\=]*)$/;
 
     this.events = {
